312afdb079139b7def987be026e72a86032de516
Frank  D'Angelo <08fdange@gmail.com>
2020-03-05 23:58:49 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song 
+  attr_accessor :artist, :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist
+    @artist 
+  end
+  
+  def artist_name
+    self.artist.name
+  end
+
+end
